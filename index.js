5875ca67d1f90a3ec8bd29c747b595ae2d4f7f87
Brandon Lim <brandonlim8890@gmail.com>
2021-03-28 13:51:45 +0800

    Fix error handling with regards to validation.

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(express.json())
 app.use(cors())
 app.use(express.static('build'))
 
-app.get(`/api/persons/:id`, (req, res) => {
+app.get(`/api/persons/:id`, (req, res, next) => {
 	Person.findById(req.params.id)
 		.then((person) => {
 			res.json(person)
@@ -24,7 +24,7 @@ app.get(`/api/persons/:id`, (req, res) => {
 		.catch((err) => next(err))
 })
 
-app.delete(`/api/persons/:id`, (req, res) => {
+app.delete(`/api/persons/:id`, (req, res, next) => {
 	Person.findByIdAndDelete(req.params.id)
 		.then(() => {
 			res.status(204).end()
@@ -48,40 +48,35 @@ app.get(`/info`, (req, res) => {
 app.post(`/api/persons`, (req, res, next) => {
 	const body = req.body
 
-	if (!body.name) {
-		return res.status(400).json({ error: 'name missing' })
-	} else if (!body.number) {
-		return res.status(400).json({ error: 'number missing' })
-	} else {
-		Person.find({ name: body.name }).then((person) => {
-			const newPerson = new Person({
-				name: body.name,
-				number: body.number
-			})
-			newPerson
-				.save()
-				.then((result) => {
-					res.json(result)
-				})
-				.catch((err) => {
-					next(err)
-				})
+	const person = new Person({
+		name: body.name,
+		number: body.number
+	})
+
+	person
+		.save()
+		.then((savedPerson) => {
+			res.json(savedPerson)
 		})
-	}
+		.catch((err) => next(err))
 })
 
 app.put(`/api/persons/:id`, (req, res) => {
-	const newPerson = req.body
-	Person.findByIdAndUpdate(newPerson.id, newPerson).then((result) => {
-		res.status(204).end()
-	})
+	Person.findByIdAndUpdate(newPerson.id, newPerson)
+		.then((result) => {
+			res.status(204).end()
+		})
+		.catch((err) => {
+			console.log(err)
+		})
 })
 
 const errorHandler = (err, req, res, next) => {
-	console.error(err.message)
-
+	console.log(err.message)
 	if (err.name === 'CastError') {
-		return response.status(400).send({ error: 'malformatted id' })
+		return res.status(400).send({ error: 'malformatted id' })
+	} else if (err.name === 'ValidationError') {
+		return res.status(400).json({ error: err.message })
 	}
 	next(err)
 }
